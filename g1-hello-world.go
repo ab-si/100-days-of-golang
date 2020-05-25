5b837e8590efce334e0e666ebeb383879a8b5b82
ab-si <abhisingh7294@gmail.com>
2020-05-25 10:12:02 +0530

    Hello world

---
+++ g1-hello-world.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+// Hello world exercise. Just run it in your terminal.
+// Use "go run g1-hello-world.go" to execute this program.
+// We will start discussion on each of the code aspects from next exercise.
+func main() {
+	fmt.Println("Hello World!")
+}
